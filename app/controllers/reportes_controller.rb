998583b58d285276287a5a8bd6b9860503913d19
stevenrojascr <stevenrojascr@gmail.com>
2011-02-25 22:10:40 -0600

    utilizando extract en lugar de strftime para heroku

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -17,11 +17,20 @@ class ReportesController < ApplicationController
   end
   
   def parejas_aniversarios_mes
-    @parejas_aniv_matrimonio = Pareja.find(:all, :conditions => ["activo='t' and STRFTIME('%m', fch_matrimonio) = ?", Date.today.strftime("%m")])
-    @parejas_aniv_el = Pareja.find(:all, :conditions => ["activo='t' and STRFTIME('%m', fch_nac_el) = ?", Date.today.strftime("%m")])
-    @parejas_aniv_ella = Pareja.find(:all, :conditions => ["activo='t' and STRFTIME('%m', fch_nac_ella) = ?", Date.today.strftime("%m")])
+    mes = Date.today.month
+    @parejas_aniv_matrimonio = Pareja.find(:all, :conditions => ["activo='t' and (EXTRACT(month FROM 'fch_matrimonio') = ?)", mes])
+    @parejas_aniv_el = Pareja.find(:all, :conditions => ["activo='t' and (EXTRACT(month FROM 'fch_nac_el') = ?)", mes])
+    @parejas_aniv_ella = Pareja.find(:all, :conditions => ["activo='t' and (EXTRACT(month FROM 'fch_nac_ella') = ?)", mes])
 
   end
+#EXTRACT(month FROM "date") = 2
+  
+#def parejas_aniversarios_mes
+#  @parejas_aniv_matrimonio = Pareja.find(:all, :conditions => ["activo='t' and STRFTIME('%m', fch_matrimonio) = ?", Date.today.strftime("%m")])
+#  @parejas_aniv_el = Pareja.find(:all, :conditions => ["activo='t' and STRFTIME('%m', fch_nac_el) = ?", Date.today.strftime("%m")])
+#  @parejas_aniv_ella = Pareja.find(:all, :conditions => ["activo='t' and STRFTIME('%m', fch_nac_ella) = ?", Date.today.strftime("%m")])
 
+# end
+  
   
 end
